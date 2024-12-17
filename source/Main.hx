f4eb10d555661ec930e15a2b8a73155bc590f3ea
Joalor64 <rhgarcon@gmail.com>
2024-12-17 15:40:34 -0500

    fix

--- source/Main.hx
+++ source/Main.hx
@@ -4,7 +4,7 @@ class Main extends openfl.display.Sprite {
 	public final config:Dynamic = {
 		gameDimensions: [1280, 720], // Width + Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 		framerate: 60, // How many frames per second the game should run at.
-		initialState: states.PlayState, // is the state in which the game will start.
+		initialState: state.PlayState, // is the state in which the game will start.
 		skipSplash: false, // Whether to skip the flixel splash screen that appears in release mode.
 		startFullscreen: false // Whether to start the game in fullscreen on desktop targets'
 	};
